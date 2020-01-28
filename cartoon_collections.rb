e1ebe9113bd2a1029a9436ee09d3770d5fb5442c
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-01-28 23:36:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,10 +14,12 @@ array
 end
 
 def long_planeteer_calls(array)
-If array.any? (false)
+array.any?{|i| array.length > 4}
+if i == false
 false
 else 
   true
+end
 end
 
 def find_the_cheese# code an argument here
