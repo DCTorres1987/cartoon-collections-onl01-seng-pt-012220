d4e7ca686ad67f10ae41b92a77f64b3896568e5d
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-01-29 01:45:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,5 +22,5 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find(cheese_types) do |word|
+  array.find(cheese_types)
 end
